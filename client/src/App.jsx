394a6375a007afc1feec790a24e2537594dedee6
Joshua Trujillo <Jtriley15@gmail.com>
2022-01-14 12:32:52 -0500

    init contract and client

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+    return (
+        <div className='app'>
+            <h1>Good Hacking 🤘</h1>
+        </div>
+    )
+}
---
+++ client/src/helpers/superfractionalizer.js
@@ -0,0 +1,1 @@
+// This is where the super fractionalizer interactions should ideally take place.
